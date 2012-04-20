c93967a024df645dada77e350e37e89c733dc1ed
woogi <bumwoogi@gmail.com>
2012-04-20 09:00:35 +0900

    add prompt dlg when saving data. change service catalog data.

--- sampleServiceCategories.js
+++ sampleServiceCategories.js
@@ -2,7 +2,9 @@
 		     							"triggers":[\
 		     							{"title":"온도감지",\
 		     							"uri":"meter/thermometer/temp/",\
-		     							"arguments":[ {"key":"30C<=","description":"섭씨 30도 이상 일때"},\
+		     							"arguments":[\
+		     										{"key":"30C<=","description":"섭씨 30도 이상 일때"},\
+		     										{"key":"25C<=","description":"섭씨 25도 이상 일때"},\
 		     										{"key":"20C<=","description":"섭씨 20도 이상 일때"},\
 		     										{"key":"10C<=","description":"섭씨 10도 이상 일때"},\
 		     										{"key":"0C<=","description":"섭씨 0도 이상 일때"},\
@@ -12,24 +14,31 @@
 		     							},\
 		     							{"title":"습도감지",\
 		     							"uri":"meter/hygrometer/humidity/",\
-		     							"arguments":[ {"key":"50%<=","description":"습도 50% 이상 일때"},\
+		     							"arguments":[\
+		     										{"key":"70%<=","description":"습도 70% 이상 일때"},\
+		     										{"key":"50%<=","description":"습도 50% 이상 일때"},\
+		     										{"key":"50%>","description":"습도 50% 미만 일때"},\
 		     										{"key":"40%<=","description":"습도 40% 이상 일때"},\
+		     										{"key":"40%>","description":"습도 40% 미만 일때"},\
 		     										{"key":"30%<=","description":"습도 30% 이상 일때"},\
-		     									    {"key":"20%>=","description":"습도 20% 이상 일때"},\
-				 							  		{"key":"10%<=","description":"습도 10% 이상 일때"}\
+		     										{"key":"30%>","description":"습도 30% 미만 일때"},\
+		     									    {"key":"20%<=","description":"습도 20% 이상 일때"},\
+		     									    {"key":"20%>","description":"습도 20% 미만 일때"},\
+				 							  		{"key":"10%>","description":"습도 10% 미만 일때"}\
 				 							]\
 				 						}\
 			 						],\
 			 						"actions":[]\
 			 						},\
-			 						{	"title":"난방 controller",\
+			 						{	"title":"히터",\
 			 							"triggers":[],\
 							 			"actions":[\
-							 			{"title":"희망 온도 제어" , \
+							 			{"title":"난방 세기 제어" , \
 							 			"uri": "controller/heater/temp/"	,\
-							 			"arguments":[{"key":"27C","description":"27도로조정"},	\
-							 						{"key":"25C","description":"25도로 조정"},\
-							 						{"key":"20C","description":"20도로 조정"}\
+							 			"arguments":[\
+							 						{"key":"high","description":"난방 강도 강으로 조정"},	\
+							 						{"key":"medium","description":"난방 강도 중으로 조정"},	\
+							 						{"key":"low","description":"난방 강도 약으로 조정"}\
 							 						]\
 										},\
 							 			{"title":"전원 제어" ,	\
